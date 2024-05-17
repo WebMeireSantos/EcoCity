8ed1bad8a4d9ac334e5928431a820bc0d09dcc83
caique-deodato <68817713+caique-deodato@users.noreply.github.com>
2024-05-16 23:16:55 -0300

    Adicionado XP/Vida + metodos + loop de jogo na fase 1

--- src/Personagem.java
+++ src/Personagem.java
@@ -1,45 +1,17 @@
 public class Personagem {
-    private String nome;
-    private double vida = 100;
-    private double forca;
-    private double resistencia;
+    private int vida = 100;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getVida() {
+    public int getVida() {
         return vida;
     }
 
     // Define um valor absoluto para a vida do personagem
-    public void setVida(Double vida) {
+    public void setVida(int vida) {
         this.vida = vida;
     }
 
     // Incrementa ou decrementa a vida do personagem
-    public void mudaVida(Double qtd) {
-        this.vida = this.vida + qtd;
+    public void diminuiVida(int qtd) {
+        this.vida = this.vida - qtd;
     }
-
-    public Double getForca() {
-        return forca;
-    }
-
-    public void setForca(Double forca) {
-        this.forca = forca;
-    }
-
-    public Double getResistencia() {
-        return resistencia;
-    }
-
-    public void setResistencia(Double resistencia) {
-        this.resistencia = resistencia;
-    }
-
 }
